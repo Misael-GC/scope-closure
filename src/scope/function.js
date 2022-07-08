09f6e43756bd3d5d6c7bb4c4f45080f110cf219f
Misael-GC <misael.gomez1623@gmail.com>
2022-07-07 21:57:30 -0500

    function and block scop

---
+++ src/scope/function.js
@@ -0,0 +1,13 @@
+function greeting(){
+    let useName = 'Ana';
+    console.log(useName);
+
+    if(useName === 'Ana'){
+        console.log(`Hello ${useName}!!!`)
+    }
+    useName === "Ana" ? console.log('80') : console.log('error');
+}
+
+greeting();
+
+console.log(useName)
